5f0789f76abfc526d5d69224d23376eb825f11a5
Gabriel Carvalho <85264276+bieltanaka11@users.noreply.github.com>
2023-05-11 02:08:34 -0300

    ProjetoVirusZombie.java

--- ProjetoVirusZombie.java
+++ ProjetoVirusZombie.java
@@ -1,26 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package projetoviruszombie;
 
+package ProjetoVirusZombie;
+
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
+/**
+ * Classe main do tipo projeto virus zumbi.
+ * @author Gabriel Carvalho 
+ * 
+ */
 public class ProjetoVirusZombie {
-    //public static final String ANSI_RESET = "\u001B[0m";
-    //public static final String ANSI_RED = "\u001B[31m";
-    //public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        //System.out.println(ANSI_RED + ANSI_GREEN_BACKGROUND);
-        //System.out.println(ANSI_GREEN_BACKGROUND + "Outro texto");
-    //}
-    Mundo m = new Mundo();
-    while(true){
-        m.desenhaMundo();
-        m.atualizaMundo();
-        try {
-            Thread.sleep(300);
-        } catch(Exception e){
-             e.printStackTrace();
+        Mundo m = new Mundo();
+        
+    UIManager UI=new UIManager();
+    
+    UI.put("OptionPane.background", Color.white);
+    UI.put("Panel.background", Color.white);
+
+
+        
+        ImageIcon iconSaudaveis = new ImageIcon("src\\ProjetoVirusZombie\\saudaveis.png");
+        ImageIcon iconMorte = new ImageIcon("src\\ProjetoVirusZombie\\caixao.png");
+        ImageIcon iconZumbi = new ImageIcon("src\\ProjetoVirusZombie\\zumbi.jpg");
+        
+        int result = JOptionPane.showConfirmDialog((Component) null, "BEM VINDO!!\nOK para começar\nCANCEL para cancelar",
+        "Simulação Transmissão Vírus Zumbi", JOptionPane.OK_CANCEL_OPTION,0, iconZumbi);
+        
+        if(result == 0){
+        switch (m.desenhaMundo()) {
+        
+        case 1:
+        JOptionPane.showMessageDialog(null,"Não há mais pessoas infectadas","Atenção", 0, iconSaudaveis);
+        break;
+        case 2:
+        JOptionPane.showMessageDialog(null,"Não há mais pessoas saudáveis","Atenção",0,iconMorte);
+        break;
+        default:
+        break;
+        
         }
+        }
+        else if(result == 2){
+        System.out.println("A simulção foi cancelada!!!!");
+        }
+        }
+
     }
-}
-}
+
+
+    
+            
+
+

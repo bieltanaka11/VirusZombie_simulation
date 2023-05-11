309d915488dc077203c5b574838b51fdea17344b
Gabriel Carvalho <85264276+bieltanaka11@users.noreply.github.com>
2023-05-11 01:43:50 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetoviruszombie;
+
+/**
+ *
+ * @author unifgcarvalho
+ */
+public class Pessoa {
+    Integer x;
+    Integer y;
+    Integer cor;
+
+}
---
+++ ProjetoVirusZombie.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projetoviruszombie;
+
+/**
+ *
+ * @author unifgcarvalho
+ */
+public class ProjetoVirusZombie {
+    //public static final String ANSI_RESET = "\u001B[0m";
+    //public static final String ANSI_RED = "\u001B[31m";
+    //public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static void main(String[] args) {
+        //System.out.println(ANSI_RED + ANSI_GREEN_BACKGROUND);
+        //System.out.println(ANSI_GREEN_BACKGROUND + "Outro texto");
+    //}
+    Mundo m = new Mundo();
+    while(true){
+        m.desenhaMundo();
+        m.atualizaMundo();
+        try {
+            Thread.sleep(300);
+        } catch(Exception e){
+             e.printStackTrace();
+        }
+    }
+}
+}

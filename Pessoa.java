7adf2a61176580acf5399ccd6c280730b066dcb9
Gabriel Carvalho <85264276+bieltanaka11@users.noreply.github.com>
2023-05-11 02:07:09 -0300

    Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,16 +1,83 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package projetoviruszombie;
+package ProjetoVirusZombie;
 
 /**
- *
- * @author unifgcarvalho
+ * Classe para objetos do tipo Pessoa.
+ * @author Gabriel Carvalho 
+ * 
  */
-public class Pessoa {
-    Integer x;
-    Integer y;
-    Integer cor;
+public abstract class Pessoa {
+
+
+    private int x;
+    private int y;
+    private int cor;
+
+    /**
+     * Método construtor
+     */
+    public Pessoa() {
+    }
+
+    /**
+     * Método construtor
+     *
+     * @param x 
+     * @param y 
+     * @param cor 
+     * mundo
+     */
+    public Pessoa(int x, int y, int cor) {
+        this.x = x;
+        this.y = y;
+        this.cor = cor;
+    }
+
+
+    public int getX() {
+        return x;
+    }
+
+
+    public void setX(int x) {
+        if(x>59)
+        this.x = 0;
+
+        else if(x<0)
+        this.x = 59;
+
+        else
+        this.x = x;
+    }
+
+
+    public int getY() {
+        return y;
+    }
+
+
+    public void setY(int y) {
+        if(y>29)
+        this.y = 0;
+
+        else if(y<0)
+        this.y = 29;
+
+        else
+        this.y = y;
+
+    }
+
+
+    public int getCor() {
+        return cor;
+    }
+
+
+    public void setCor(int cor) {
+        this.cor = cor;
+    }
+
+
 
 }
+
